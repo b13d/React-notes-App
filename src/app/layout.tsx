a370e0b874c454229e180b14ebc4b2a2274ac373
b13d <4.topppp@mail.ru>
2023-05-19 00:00:06 +0400

    small fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-"use client"
+"use client";
 
-import './globals.css'
-import {Provider} from "react-redux";
-import "./globals.css"
-import {store} from "@/app/store";
-
+import "./globals.css";
+import { Provider } from "react-redux";
+import "./globals.css";
+import { store } from "@/app/store";
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <Provider store={store}>
-            <html lang="en">
-            <body>{children}</body>
-            </html>
-        </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <html lang="en">
+        <head>
+          <title>notes-app</title>
+        </head>
+        <body>{children}</body>
+      </html>
+    </Provider>
+  );
 }
-
